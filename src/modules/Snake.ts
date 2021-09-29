ddb47af17983928ce5afe4f5f7d179444222a2c4
Calvin <zxw880507@gmail.com>
2021-09-29 11:02:35 -0600

    add snake module

---
+++ src/modules/Snake.ts
@@ -0,0 +1,30 @@
+class Snake {
+  element: HTMLElement;
+  head: HTMLElement;
+  bodies: HTMLCollection;
+  constructor() {
+    this.element = document.getElementById("snake")!;
+    this.head = document.querySelector("#snake > div")! as HTMLElement;
+    this.bodies = this.element.getElementsByTagName("div");
+  }
+
+  get X() {
+    return this.head.offsetLeft;
+  }
+  get Y() {
+    return this.head.offsetTop;
+  }
+
+  set X(value: number) {
+    this.head.style.left = `${value}px`;
+  }
+  set Y(value: number) {
+    this.head.style.top = `${value}px`;
+  }
+
+  addBody() {
+    this.element.insertAdjacentHTML("beforeend", "<div></div>");
+  }
+}
+
+export default Snake;

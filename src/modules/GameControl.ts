7d915bf26796fcaf7c3cb715821cd1f840d2e6fb
Calvin <zxw880507@gmail.com>
2021-09-29 14:24:55 -0600

    add methods to gameControl

--- src/modules/GameControl.ts
+++ src/modules/GameControl.ts
@@ -6,6 +6,8 @@ class GameControl {
   snake: Snake;
   food: Food;
   scorePanel: ScorePanel;
+  direction: string = "";
+  isAlive: boolean = false;
   constructor() {
     this.snake = new Snake();
     this.food = new Food();
@@ -14,11 +16,59 @@ class GameControl {
   }
 
   init() {
-    document.addEventListener("keydown", this.keydownHandler);
+    this.isAlive = true;
+    document.addEventListener("keydown", this.keydownHandler.bind(this));
+    this.move();
   }
 
   keydownHandler(event: KeyboardEvent) {
-    console.log(event.key);
+    this.direction = event.key;
+  }
+  move() {
+    let X = this.snake.X;
+    let Y = this.snake.Y;
+
+    switch (this.direction) {
+      case "ArrowUp":
+      case "Up":
+        Y -= 10;
+        break;
+      case "ArrowDown":
+      case "Down":
+        Y += 10;
+        break;
+      case "ArrowLeft":
+      case "Left":
+        X -= 10;
+        break;
+      case "ArrowRight":
+      case "Right":
+        X += 10;
+        break;
+      default:
+        break;
+    }
+
+    this.checkEat(X, Y);
+
+    try {
+      this.snake.X = X;
+      this.snake.Y = Y;
+    } catch (error: any) {
+      alert(error.message + " GAME OVER!");
+      this.isAlive = false;
+    }
+
+    this.isAlive &&
+      setTimeout(this.move.bind(this), 300 - (this.scorePanel.level - 1) * 30);
+  }
+
+  checkEat(X: number, Y: number) {
+    if (X === this.food.X && Y === this.food.Y) {
+      this.food.change();
+      this.scorePanel.addScore();
+      this.snake.addBody();
+    }
   }
 }
 
